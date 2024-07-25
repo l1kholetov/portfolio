5118f912bd4ec00abe32071ef2bdffb5ac06abec
Vladimir <likholetov.vladimir2@yandex.ru>
2024-07-25 13:09:07 +0300

    add section about. modify sidepanel including the corresponding script

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 			menu = document.querySelector('.menu'),
 			menuClose = document.querySelector('.menu__close'),
-			menuOverlay = document.querySelector('.menu__overlay');
+			menuOverlay = document.querySelector('.menu__overlay'),
+			sidepanel = document.querySelector('.sidepanel');
 
 hamburger.addEventListener('click', () => {
 	menu.classList.add('menu__active');
@@ -18,4 +19,12 @@ modalClose(menuOverlay);
 
 window.addEventListener('keydown', (e) => {
 	if (e.keyCode == '27') menu.classList.remove('menu__active');
+});
+
+window.addEventListener('scroll', () => {
+	if (scrollY >= 450) {
+		sidepanel.classList.add('sidepanel__black')
+	} else {
+		sidepanel.classList.remove('sidepanel__black');
+	}
 });
