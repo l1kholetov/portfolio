80d0dae5b01925fba1cc6fb55804a4807fd7b7f5
Vladimir <likholetov.vladimir2@yandex.ru>
2024-07-29 14:29:13 +0300

    modified script.js. improved user interaction with navigation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,33 +1,50 @@
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger'),
 			menu = document.querySelector('.menu'),
 			menuClose = document.querySelector('.menu__close'),
 			menuOverlay = document.querySelector('.menu__overlay'),
+			menuLink = document.querySelectorAll('.menu__link'),
 			sidepanel = document.querySelector('.sidepanel'),
+			promo = document.querySelector('.promo'),
 			skillsValue = document.querySelectorAll('.skills__item-value'),
 			skillsBar = document.querySelectorAll('.skills__item-bar');
 
 hamburger.addEventListener('click', () => {
-	menu.classList.add('menu__active');
+	menu.classList.add('menu_active');
+	document.body.style.overflow = 'hidden';
+});
+
+window.addEventListener('scroll', () => {
+	if (scrollY >= promo.scrollHeight - 20) {
+		hamburger.classList.add('hamburger_black')
+	} else {
+		hamburger.classList.remove('hamburger_black');
+	}
 });
 
 function modalClose(sel) {
 	sel.addEventListener('click', () => {
-		menu.classList.remove('menu__active');
+		menu.classList.remove('menu_active');
+		document.body.style.overflow = '';
 	});
 }
 
 modalClose(menuClose);
 modalClose(menuOverlay);
 
+menuLink.forEach(item => {
+	modalClose(item);
+});
+
 window.addEventListener('keydown', (e) => {
-	if (e.keyCode == '27') menu.classList.remove('menu__active');
+	if (e.keyCode == '27') menu.classList.remove('menu_active');
+	document.body.style.overflow = '';
 });
 
 window.addEventListener('scroll', () => {
-	if (scrollY >= 450) {
-		sidepanel.classList.add('sidepanel__black')
+	if (scrollY >= promo.scrollHeight - 230) {
+		sidepanel.classList.add('sidepanel_black')
 	} else {
-		sidepanel.classList.remove('sidepanel__black');
+		sidepanel.classList.remove('sidepanel_black');
 	}
 });
 
