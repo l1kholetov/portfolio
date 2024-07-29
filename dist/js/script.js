a84289bc31e9dcb9faa602ee3ffb94d09440e9d2
Vladimir <likholetov.vladimir2@yandex.ru>
2024-07-29 13:33:42 +0300

    add smooth scroll for local links

--- dist/js/script.js
+++ dist/js/script.js
@@ -34,3 +34,24 @@ window.addEventListener('scroll', () => {
 skillsValue.forEach( (item, i) => {
 	skillsBar[i].style.width = item.innerHTML;
 });
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+	link.addEventListener('click', function(e) {
+			e.preventDefault();
+
+			let href = this.getAttribute('href').substring(1);
+
+			const scrollTarget = document.getElementById(href);
+
+			// const topOffset = document.querySelector('.scrollto').offsetHeight;
+			const topOffset = 0;
+			const elementPosition = scrollTarget.getBoundingClientRect().top;
+			const offsetPosition = elementPosition - topOffset;
+
+			window.scrollBy({
+					top: offsetPosition,
+					behavior: 'smooth'
+			});
+	});
+});
