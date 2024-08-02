2181895dfbe44cec49866c62892efb8991dd9705
Vladimir <likholetov.vladimir2@yandex.ru>
2024-08-02 16:29:35 +0300

    add JS project Food to project list

---
+++ dist/projects/food/js/modules/cards.js
@@ -0,0 +1,70 @@
+import { getResource } from "../services/services";
+
+function cards() {
+  // Используем классы для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 91;
+      this.changeToRUB();
+    }
+
+    changeToRUB() {
+      this.price *= this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+          <img src=${this.src} alt=${this.alt}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+          </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+
+  fetch ('db.json')
+    .then(data => data.json())
+    .then(res => {
+      res.menu.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
+    });
+
+  // getResource('http://localhost:3000/menu')
+  //   .then(data => {
+  //     data.forEach(({img, altimg, title, descr, price}) => {
+  //       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //     });
+  //   });
+
+  // axios.get('http://localhost:3000/menu')
+  //   .then(data => {
+  //     data.data.forEach(({img, altimg, title, descr, price}) => {
+  //       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //     });
+  //   });
+}
+
+export default cards;
---
+++ dist/projects/food/js/script.js
@@ -0,0 +1,33 @@
+var Promise = require('es6-promise-polyfill').Promise;
+import 'nodelist-foreach-polyfill';
+
+import tabs from  './modules/tabs';
+import modal from  './modules/modal';
+import timer from  './modules/timer';
+import cards from  './modules/cards';
+import calc from  './modules/calc';
+import forms from  './modules/forms';
+import slider from  './modules/slider';
+import { openModal } from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
+
+  tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+  modal('[data-modal]', '.modal', modalTimerId);
+  timer('.timer', '2024-05-31');
+  slider({
+    container: '.offer__slider',
+    nextArrow: '.offer__slider-next',
+    prevArrow: '.offer__slider-prev',
+    totalCounter: '#total',
+    currentCounter: '#current',
+    wrapper: '.offer__slider-wrapper',
+    field: '.offer__slider-inner',
+    slide: '.offer__slide'
+  });
+  forms('form', modalTimerId);
+  calc();
+  cards();
+});
